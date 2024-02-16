92ba7143d6f6f0241723c85299a442fa5293b89a
Guangju Yao <guangju.yao.me@gmail.com>
2024-02-17 04:31:16 +0800

    接口测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import Head from "next/head";
-import Link from "next/link";
 
 import { api } from "~/utils/api";
+import {useMutation} from "@tanstack/react-query";
+import {router} from "next/client";
 
 export default function Home() {
+
   const hello = api.post.hello.useQuery({ text: "from tRPC" });
+
+
+
+  const { mutate, error } = api.post.create.useMutation();
+
 
   return (
     <>
@@ -18,34 +25,20 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
           <p className="text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
         </div>
+          <form onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            // @ts-ignore
+            mutate({ name: formData.get('name') });
+          }}>
+            <input name="name" />
+            <button>提交</button>
+          </form>
+
       </main>
     </>
   );
